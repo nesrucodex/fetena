208754a938b6f2c6e9e205e0518130e2f2f47e33
Nesredin Getahun <nesrucodex01@gmail.com>
2024-01-15 21:47:40 +0300

    Question maker and selling project frontend working on making questions

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Application from "./Fetena Application/Application";
+import "./app.css";
+function App() {
+  return (
+    <div className="app">
+      <Application />
+    </div>
+  );
+}
+
+export default App;
